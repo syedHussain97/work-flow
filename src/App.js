2a878bb2381a40865af227520459dd988985ebba
Syed Hussain <syed.m.husyn@gmail.com>
2020-09-11 15:34:49 +1200

    adding npm run test to service

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello hello a <code>src/App.js</code> and save to reload.
+          Hello hello aa <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
